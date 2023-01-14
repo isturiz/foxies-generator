0422a0d4ddc45359c2311b7564d25ca5dd98ba34
isturiz <isturizmauricio@gmail.com>
2023-01-14 14:03:59 -0400

    add lazy loading

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
-console.log('Happy hacking :)')
-  // < div class="p-4" >
-  //   <img
-  //     class="mx-auto"
-  //     width="320px"
-  //     src="https://randomfox.ca/images/91.jpg"
-  //     alt="">
-  //   </>
+import { registerImg } from "./lazy";
 
 const max = 123
 const min = 1
@@ -24,7 +12,7 @@ const createImageNode = () => {
   const img = document.createElement('img')
   img.className = 'mx-auto'
   img.width = '320'
-  img.src = `https://randomfox.ca/images/${random()}.jpg`
+  img.dataset.src = `https://randomfox.ca/images/${random()}.jpg`
 
   container.appendChild(img)
 
@@ -39,6 +27,7 @@ const addButton = document.querySelector('button')
 const addImg = () => {
   const newImg = createImageNode()
   mountNode.append(newImg)
+  registerImg(newImg)
 }
 
 addButton.addEventListener('click', addImg)
